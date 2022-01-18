7ea7bb1867ef0b707bc1acae48790b2d17a283a5
Airels <yohan13240@hotmail.fr>
2022-01-18 10:23:09 +0100

    Cell.cpp implémenté

--- cell.cpp
+++ cell.cpp
@@ -1,6 +1,35 @@
 #include "cell.h"
 
-cell::cell()
-{
+Cell::Cell(QString text, QColor color) {
+    this->text = text;
+    this->color = color;
+}
 
+Cell::Cell(QString text) {
+    this->text = text;
+    this->color = QColor(255, 255, 255);
+}
+
+Cell::~Cell() {
+
+}
+
+QString Cell::getText() {
+    return text;
+}
+
+void Cell::setText(QString text) {
+    this->text = text;
+}
+
+QColor Cell::getColor() {
+    return this->color;
+}
+
+void Cell::setColor(QColor color) {
+    this->color = color;
+}
+
+bool Cell::isUpdatable() {
+    return this->updatable;
 }

6ac4328f312592057b466d246c20647dd4363a75
Titouan Bricout <titouan.bricout@etu.univ-amu.fr>
2022-01-25 16:11:40 +0100

    correction : dataManager now passed as a true reference

--- filemanager.cpp
+++ filemanager.cpp
@@ -11,7 +11,7 @@ FileManager::FileManager(QWidget * attachedWidget)
     this->attachedWidget = attachedWidget;
 }
 
-bool FileManager::openFile(DataManager *dataManager) {
+bool FileManager::openFile(DataManager *&dataManager) {
     if (dataManager == nullptr)
         dataManager = new DataManager();
 

9ea076d55cdc0c622c6d33dd89d344b319e85850
Airels <yohan13240@hotmail.fr>
2022-02-01 10:09:53 +0100

    déclaration save par image

--- filemanager.h
+++ filemanager.h
@@ -13,6 +13,7 @@ class FileManager
 public:
     const QString acceptedOpenFileTypes = "CSV/ITabs File (*.csv *.itabs)";
     const QString acceptedSaveFileTypes = "ITabs File (*.itabs)";
+    const QString acceptedSaveImageTypes = "Bitmap (*.bmp);;PNG Image (*.png);;JPG Image (*.jpg)";
 
     FileManager(QWidget * attachedWidget);
     bool openFile(DataManager *&dataManager);

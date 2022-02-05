a36fbaef6caca2f0e5f76f4ac1a644736873a7b7
Eddy Ikhlef <eddy.ikhlef@protonmail.com>
2022-02-05 11:17:37 +0100

    documentation for modulocolordialog.h

--- modulocolordialog.h
+++ modulocolordialog.h
@@ -3,11 +3,20 @@
 
 #include "ui_modulocolordialog.h"
 
+/**
+ * @brief The ModuloColorDialog class is the one in charge of the QDialog to handle the "modulo color" filter.
+ * @author Eddy IKHLEF
+ */
 class ModuloColorDialog : public QDialog, private Ui::ModuloColorDialog
 {
     Q_OBJECT
 
 public:
+    /**
+     * @brief Constructor of the class. Have to be linked with the followings parameters:
+     * @param parent it's parent QWidget.
+     * @author Eddy IKHLEF
+     */
     explicit ModuloColorDialog(QWidget *parent = nullptr);
     ~ModuloColorDialog();
     QColor getSelectedColor();

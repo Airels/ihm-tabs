94272d5f521e3922b4869acad793300da3cceb1a
Eddy Ikhlef <eddy.ikhlef@protonmail.com>
2022-02-05 11:17:28 +0100

    documentation for gradientcolordialog.h

--- gradientcolordialog.h
+++ gradientcolordialog.h
@@ -3,11 +3,20 @@
 
 #include "ui_gradientcolordialog.h"
 
+/**
+ * @brief The GradientColorDialog class is the one in charge of the QDialog to handle the "gradient color" filter.
+ * @author Eddy IKHLEF
+ */
 class GradientColorDialog : public QDialog, private Ui::GradientColorDialog
 {
     Q_OBJECT
 
 public:
+    /**
+     * @brief Constructor of the class. Have to be linked with the followings parameters:
+     * @param parent it's parent QWidget.
+     * @author Eddy IKHLEF
+     */
     GradientColorDialog(QWidget *parent = nullptr);
     ~GradientColorDialog();
     QColor getSelectedColorMin();

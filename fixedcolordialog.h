49598c904a232f33b0ab716f58ed56b00354875e
Eddy Ikhlef <eddy.ikhlef@protonmail.com>
2022-02-05 11:17:17 +0100

    documentation for fixedcolordialog.h

--- fixedcolordialog.h
+++ fixedcolordialog.h
@@ -3,11 +3,20 @@
 
 #include "ui_fixedcolordialog.h"
 
+/**
+ * @brief The FixedColorDialog class is the one in charge of the QDialog to handle the "fixed color" filter.
+ * @author Eddy IKHLEF
+ */
 class FixedColorDialog : public QDialog, private Ui::FixedColorDialog
 {
     Q_OBJECT
 
 public:
+    /**
+     * @brief Constructor of the class. Have to be linked with the followings parameters:
+     * @param parent it's parent QWidget.
+     * @author Eddy IKHLEF
+     */
     FixedColorDialog(QWidget *parent = nullptr);
     ~FixedColorDialog();
     QColor getSelectedColor();

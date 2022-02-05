2a6d271a33808b67c0765fe95ff3c8cee2e9c880
Eddy Ikhlef <eddy.ikhlef@protonmail.com>
2022-02-05 11:15:54 +0100

    documentation for conditionalcolordialog.h

--- conditionalcolordialog.h
+++ conditionalcolordialog.h
@@ -3,11 +3,20 @@
 
 #include "ui_conditionalcolordialog.h"
 
+/**
+ * @brief The ConditionalColorDialog class is the one in charge of the QDialog to handle the "conditional color" filter.
+ * @author Eddy IKHLEF
+ */
 class ConditionalColorDialog : public QDialog, private Ui::ConditionalColorDialog
 {
     Q_OBJECT
 
 public:
+    /**
+     * @brief Constructor of the class. Have to be linked with the followings parameters:
+     * @param parent it's parent QWidget.
+     * @author Eddy IKHLEF
+     */
     explicit ConditionalColorDialog(QWidget *parent = nullptr);
     ~ConditionalColorDialog();
     QColor getSelectedColorMin();

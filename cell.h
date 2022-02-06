5b02b8ef6331c5d1e3f12e44d00a0e742d76d2a9
Eddy Ikhlef <eddy.ikhlef@protonmail.com>
2022-02-06 19:30:07 +0100

    documentation for cell.h

--- cell.h
+++ cell.h
@@ -5,20 +5,67 @@
 #include <QApplication>
 #include <QStandardItem>
 
+/**
+ * @brief The Cell class helps to work with the selected data (cell's value and cell's color).
+ *        Elements of this class will be used by DataManager and filters classes.
+ *        By doing so, we can easily retrieve the data of a cell and update them, by doing so,
+ *        correspondings fields herited from the QStandardItem will automatically be updated.
+ *
+ * @author Yohan VIZCAINO, Eddy IKHLEF
+ */
 class Cell: public QStandardItem
 {
 
 public:
+    /**
+     * @brief Constructor of the class. Allows to instanciate a cell with a specific value and color.
+     * @param value the given double value of the cell.
+     * @param color the given QColor of the cell.
+     * @author Eddy IKHLEF
+     */
     Cell(double value, QColor color);
+    /**
+     * @brief Constructor of the class. Allows to instanciate a cell with a specific value only.
+     *        The instanciated cell will have a base color defined by the implementation (white for example).
+     * @param value the given double value of the cell.
+     * @author Yohan VIZCAINO
+     */
     Cell(double value);
     ~Cell();
 
+    /**
+     * @brief getvalue
+     * @return the value of the cell.
+     * @author Eddy IKHLEF
+     */
     double getvalue();
+    /**
+     * @brief setValue
+     * @param value the new value of the cell to be set.
+     * @author Eddy IKHLEF
+     */
     void setValue(double value);
+    /**
+     * @brief getColor
+     * @return the QColor of the cell.
+     * @author Eddy IKHLEF
+     */
     QColor getColor();
+    /**
+     * @brief setColor
+     * @param color the new QColor of the cell to be set.
+     * @author Eddy IKHLEF
+     */
     void setColor(QColor color);
     bool isUpdatable();
-    // void setUpdatable(bool updatable);
+
+    /**
+     * @brief setUpdatable
+     * @param updatable boolean value to set the cell as an updatable cell (true) or not (false).
+     *        an updatable cell can have its value updated. a non updatable cell cannot have its value updated.
+     * @author Yohan VIZCAINO
+     */
+    //void setUpdatable(bool updatable);
 
 private:
     double value;
